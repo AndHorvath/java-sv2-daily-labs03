c93801943ea392aa97c2c51d9c209a2f31776020
András Horváth <horvath.andras@prevaudit.hu>
2021-10-26 15:06:05 +0200

    day02 prime has been done

---
+++ week03-lab-tasks/src/main/java/day02/Prime.java
@@ -0,0 +1,21 @@
+package day02;
+
+import java.util.Scanner;
+
+public class Prime {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean mayBePrime = true;
+        int integer;
+
+        System.out.println("Please, enter a number!");
+        integer = scanner.nextInt();
+
+        for (int i = 2; i < integer && mayBePrime; i++) {
+            mayBePrime = integer % i != 0;
+        }
+
+        System.out.println("The number you have given is" + (!mayBePrime || integer == 1 ? " not" : "") + " a prime");
+    }
+}
